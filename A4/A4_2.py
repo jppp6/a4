ec3a975b11fd6e96096ab8cb5d2c1e4f049b3dec
Jp Le Blanc <6jpleblanc@gmail.com>
2021-11-16 16:45:45 -0500

    Completed Functionality

--- A4/A4_2.py
+++ A4/A4_2.py
@@ -1,2 +1,30 @@
-for i in range(10):
-    print(i)
+"""
+This program
+
+Author: Jean-Philippe Le Blanc
+Section #: 002
+Student #: 20157203
+Date: November 15, 2021
+"""
+
+def hopping_game(n, s=""):
+    if n == 0:
+        return ["0"+s]
+    elif n == 1:
+        return ["0-1"+s]
+    else:
+        return hopping_game(n-1, f"-{n}"+s) + hopping_game(n-2, f"-{n}"+s)
+        
+        
+        
+
+        
+
+
+def main():
+    print(hopping_game(5))
+    
+    return 0
+
+# main() call to start the program
+main()
